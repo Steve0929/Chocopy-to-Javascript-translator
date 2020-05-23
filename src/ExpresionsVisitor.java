8f56f905f11f76cb18cef3c364d92ef03bf32230
afmoyar <afmoyar@unal.edu.co>
2020-05-23 11:38:58 -0500

    adding cexpr rules to visitor

--- src/ExpresionsVisitor.java
+++ src/ExpresionsVisitor.java
@@ -1,4 +1,18 @@
 public class ExpresionsVisitor extends chocogrammarBaseVisitor<String> {
+
+    public String getListElements(chocogrammarParser.CexprContext ctx) {
+        StringBuilder builder = new StringBuilder();
+        for (chocogrammarParser.ExprContext expr : ctx.expr()) {
+            builder.append(visitExpr(expr)).append(", ");
+        }
+        String arguments = builder.toString();
+        try {
+            return arguments.substring(0, arguments.length() - 2);
+        } catch (Exception e) {
+            //enters if arguments is empty
+            return arguments;
+        }
+    }
     @Override
     public String visitExpr(chocogrammarParser.ExprContext ctx) {
         StringBuilder builder= new StringBuilder();
@@ -8,7 +22,7 @@ public class ExpresionsVisitor extends chocogrammarBaseVisitor<String> {
         }else if(ctx.not_expr()!=null)
         {
             //not_expr
-            builder.append("!").append(visitExpr(ctx.expr(0)));
+            builder.append(visitNot_expr(ctx.not_expr()));
         }else if(ctx.AND()!=null)
         {
             builder.append(visitExpr(ctx.expr(0))).append(" && ").append(visitExpr(ctx.expr(1)));
@@ -21,19 +35,42 @@ public class ExpresionsVisitor extends chocogrammarBaseVisitor<String> {
 
     @Override
     public String visitCexpr(chocogrammarParser.CexprContext ctx) {
+        StringBuilder builder = new StringBuilder();
+        if(ctx.ID()!=null && ctx.TK_PAR_IZQ()==null)
+        {
+            return ctx.ID().getText();
+        }
         if(ctx.literal()!=null)
         {
             return visitLiteral(ctx.literal());
         }
-        else if(ctx.array_lenght()!=null)
+        if(ctx.TK_COR_IZQ()!=null)
+        {
+            //TK_COR_IZQ (expr (TK_COMMA expr)*)? TK_COR_DER
+            return builder.append("[").append(getListElements(ctx)).append("]").toString();
+        }
+        if(ctx.ID()==null && ctx.TK_PAR_IZQ()!=null)
+        {
+            //TK_PAR_IZQ expr TK_PAR_DER
+            return builder.append("(").append(visitExpr(ctx.expr(0))).append(")").toString();
+        }
+        if(ctx.array_lenght()!=null)
         {
             return  visitArray_lenght(ctx.array_lenght());
-        }else if(ctx.print()!=null)
+        }
+        if(ctx.print()!=null)
         {
             return visitPrint(ctx.print());
             //semantic error i think
         }
         return "";
+    }
+
+    @Override
+    public String visitNot_expr(chocogrammarParser.Not_exprContext ctx) {
+        StringBuilder builder= new StringBuilder();
+        builder.append("!").append(visitExpr(ctx.expr()));
+        return builder.toString();
     }
 
     @Override
