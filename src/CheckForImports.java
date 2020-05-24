88a2ab1cf413f0820d76e2267eeecb1f3a153163
afmoyar <afmoyar@unal.edu.co>
2020-05-24 09:54:05 -0500

    avoiding multiple imports when translating input to node

--- src/CheckForImports.java
+++ src/CheckForImports.java
@@ -1,7 +1,13 @@
 public class CheckForImports extends chocogrammarBaseListener{
+    private int counter = 0;
     @Override
     public void enterInput(chocogrammarParser.InputContext ctx) {
-        System.out.println("//please run npm i readline-sync if you havent");
-        System.out.println("var readlineSync = require('readline-sync');");
+        counter++;
+        if(counter==1)
+        {
+            System.out.println("//please run npm i readline-sync if you havent");
+            System.out.println("var readlineSync = require('readline-sync');");
+        }
+
     }
 }
